019d3f094e288f8a1ce0bc8009d7afc863be07fb
Minn05 <minh.nhan.tran2001@gmail.com>
2024-03-05 00:03:33 +0700

    part2-1st commit

---
+++ Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
@@ -0,0 +1,7 @@
+package com.ecommerce.admin.config;
+
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+public class AdminConfiguration extends WebSecurityConfigurer {
+}
