54ad9a1f9b3d9b60d174aac56581495f87cdfa34
Minn05 <minh.nhan.tran2001@gmail.com>
2024-03-06 00:08:40 +0700

    part2-2nd commit

--- Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
+++ Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
@@ -1,7 +1,27 @@
 package com.ecommerce.admin.config;
 
 
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AdminConfiguration extends WebSecurityConfigurer {
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class AdminConfiguration  {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new AdminServiceConfig();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+
 }
